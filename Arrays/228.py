990252df437fd4ca55b0cbf07d09f9ea9f0cfa74
Hunter69240 <aadishds@gmail.com>
2026-07-22 22:36:49 +0530

    228 + 414 solved

--- Arrays/228.py
+++ Arrays/228.py
@@ -1,12 +1,60 @@
+# nums = [0,1,2,4,5,7]
+
+# d={0:[]}
+# count=0
+
+# for i in nums:
+#     if i+1 in nums:
+#         d[count].append(i)
+#     else:
+#         count+=1
+#         d[count]=[]
+# print(d)
+
+
+
+#With Formating
+
 nums = [0,1,2,4,5,7]
 
-d={0:[]}
+d={
+    0:[]
+}
+res=[]
 count=0
-
 for i in nums:
-    if i+1 in nums:
-        d[count].append(i)
+    if i+1 in nums :
+        d[count].append(i)   
     else:
+        d[count].append(i)
         count+=1
         d[count]=[]
 print(d)
+
+for i in d:
+    if d[i]:
+        start=min(d[i])
+        end=max(d[i])
+        if start !=end:
+            res.append(f"{start}->{end}")
+        else:
+            res.append(f"{start}")
+print(res)
+
+
+#OR
+#Since array is sorted i just have to check forward positions as doing and in check in array is O(n)
+nums = [0,1,2,4,5,7]
+res = []
+i = 0
+n = len(nums)
+while i < n:
+        start = i
+        while i + 1 < n and nums[i+1] == nums[i] + 1:
+            i += 1
+        if start == i:
+            res.append(f"{nums[start]}")
+        else:
+            res.append(f"{nums[start]}->{nums[i]}")
+        i += 1
+print(res)
--- q.py
+++ q.py
@@ -1,8 +1,14 @@
-nums = [1,2,3,1]
-dp=[0 for i in range(len(nums)+1)]
-dp[-2]=nums[-1]
-print(dp)
-for i in range(len(nums)-2,-1,-1):
-    print(i)
-    dp[i]=max(nums[i]+dp[i+2],dp[i+1])
-print(dp)
+def a():
+    nums  = [-1,2,3]
+    
+    nums=set(nums)
+    print("Set",nums)
+    if len(nums)>2:
+        nums=list(nums)
+        nums.sort()
+        return (nums[len(nums)-3])
+    else:
+        return max(nums)
+    
+
+print(a())
